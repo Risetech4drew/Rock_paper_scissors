3d6aec3f0c099afe06d3c19c2b20e5469bd1dd93
Risetech4drew <kanyinjiandy2@gmail.com>
2023-03-18 03:34:12 +0100

    modified js code for adding class to html elem

--- index.js
+++ index.js
@@ -1,43 +1,8 @@
-//js code for buttons
-// const optBtns = document.querySelector('.options');
-// optBtns.addEventListener('click', function(event){
-//     if (event.target.matches('.rock, .paper, .scissor')){
-//         container.classList.add('active');
-//     }
-//     else
-//     {
-//         container.classList.remove('active');
-//     }
-// });
-
-// function optBtnToggle()
-// {
-//     console.log('Clicked');
-// }
-
-
-
-
-
-
-
-// const btnEl = document.getElementById('btn');
-
-// btnEl.addEventListener('click', optBtnToggle);
-
+//javascript code for bu
 const optionbtns = document.querySelectorAll('.rock, .paper, .scissor');
 
 for (const optionbtn of optionbtns){
-    optionbtn.addEventListener('click', function optionclick(event) {
-    //    if (event.target.matches('.rock, .paper, .scissor'))
-    //    {
-    //     optionbtn.classList.add('active');
-    //    }
-    //    else
-    //    {
-    //     optionbtn.classList.remove('active')
-    //    }
-
+    optionbtn.addEventListener('click', function optionclick(event) {   
         //romove 'active' class from all buttons
         optionbtns.forEach(optionbtn =>{
             optionbtn.classList.remove('active');
